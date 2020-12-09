692daf27c89e232ebf9331c509641ae40a6d962b
NicoExcalibur <nico.zanz@gmail.com>
2020-12-09 15:50:29 +0100

    :construction:create component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <contenu></contenu>
   </div>
 </template>
 
 <script>
 
+import Contenu from './components/Contenu.vue'
+
 export default {
   name: 'App',
   components: {
+    'contenu' : Contenu
   }
 }
 </script>
--- src/components/Contenu.vue
+++ src/components/Contenu.vue
@@ -1,13 +1,20 @@
 <template>
-    
+    <div>
+        {{ txt }}
+    </div>
 </template>
 
 <script>
+
 export default {
-    
+    data(){
+        return {
+            txt: 'je suis le contenu'
+        }
+    }
 }
+
 </script>
 
 <style>
-
 </style>
